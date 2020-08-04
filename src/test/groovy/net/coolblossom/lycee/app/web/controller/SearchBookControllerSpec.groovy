f3839d2d4276235205a14b84dbec411743f5883b
ryou <coolblossom.0130@gmail.com>
2020-08-04 09:07:08 +0900

    add test code

--- src/test/groovy/net/coolblossom/lycee/app/web/controller/SearchBookControllerSpec.groovy
+++ src/test/groovy/net/coolblossom/lycee/app/web/controller/SearchBookControllerSpec.groovy
@@ -1,28 +1,27 @@
 package net.coolblossom.lycee.app.web.controller
 
-import net.coolblossom.lycee.app.web.config.JpaConfig
-import net.coolblossom.lycee.app.web.config.ThymeleafConfig
-import net.coolblossom.lycee.app.web.config.WebConfig
-import net.coolblossom.lycee.app.web.repository.BookRepository
-import net.coolblossom.lycee.app.web.service.BookService
+import net.coolblossom.lycee.app.web.entity.Author
+import net.coolblossom.lycee.app.web.entity.Book
+import net.coolblossom.lycee.app.web.entity.Publisher
 import net.coolblossom.lycee.app.web.service.impl.BookServiceImpl
 import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultHandler
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import static org.hamcrest.Matchers.containsString
+import static org.hamcrest.Matchers.not
+import static org.springframework.test.web.servlet.ResultMatcher.matchAll
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -36,23 +35,91 @@ class SearchBookControllerSpec extends Specification {
     WebApplicationContext appContext
 
     @SpringBean
-    BookRepository bookRepositoryMock = Mock()
+    BookServiceImpl bookServiceMock = Mock()
 
     def setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(appContext).build()
     }
 
     @Unroll
-    def "test" () {
+    def "test empty #descr" () {
         given:
-            bookRepositoryMock.findByBookNameLike(_ as String) >> Collections.emptyList()
+        bookServiceMock.select(_ as String) >> Collections.emptyList()
+
         when:
-            def result = mockMvc.perform(MockMvcRequestBuilders.get("/search"))
+        def request = MockMvcRequestBuilders.get("/search/")
+        if (testcase.keyword != null) {
+            request.param("keyword", testcase.keyword)
+        }
+
+        def result = mockMvc.perform(request)
 
         then:
-            result.andDo(MockMvcResultHandlers.print())
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andExpect(MockMvcResultMatchers.content().contentType("text/html;charset=UTF-8"))
+        result.andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(matchAll(*testcase.expected))
 
+        where:
+        testcase << [
+                [
+                        "descr": "null",
+                        "keyword": null,
+                        "expected": [
+                                content().string(not(containsString("/search/detail/")))
+                        ]
+                ],
+                [
+                        "descr": "emptystring",
+                        "keyword": "",
+                        "expected": [
+                                content().string(not(containsString("/search/detail/")))
+                        ]
+                ]
+        ]
+        descr = testcase.descr
+    }
+
+    @Unroll
+    def "test result [#description]"() {
+        given:
+        bookServiceMock.select(_ as String) >> testcase.bookList
+
+        when:
+        def request = MockMvcRequestBuilders.get("/search/")
+        if (testcase.keyword != null) {
+            request.param("keyword", testcase.keyword)
+        }
+
+        def result = mockMvc.perform(request)
+
+        then:
+        result.andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(matchAll(*testcase.expected))
+
+        where:
+        testcase << [
+                [
+                        "description": "many result",
+                        "keyword": "猫",
+                        "bookList": [
+                                new Book(
+                                        bookId: 1,
+                                        bookName: "吾輩は猫である",
+                                        author: new Author(authorId: 1, authorName: "夏目漱石"),
+                                        publisher: new Publisher(publisherId: 1, publisherName: "ちくま文庫")
+                                )
+                        ],
+                        "expected": [
+                                content().string(containsString("value=\"猫\"")),
+                                content().string(containsString("href=\"/search/detail/1\"")),
+                                content().string(containsString("<td>夏目漱石</td>")),
+                                content().string(containsString("<td>ちくま文庫</td>")),
+                        ]
+                ]
+        ]
+        description = testcase.description
     }
 }
